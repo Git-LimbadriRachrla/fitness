1e0ac83199bacc9b25809e58ccb87dffe7918030
RACHARLA_LIMBADRI <149922892+Git-LimbadriRachrla@users.noreply.github.com>
2024-04-28 10:32:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Main from './pages/main';
+
+function App() {
+  return (
+    <div className="text-center bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
